894b4a5284f4c7eee6c4c0d5b02e4edfa7f4951a
Seth Jennings <sjenning@redhat.com>
2017-12-04 16:17:01 -0600

    add e2e-node scripts

---
+++ scripts/e2e-node-conformance.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+
+set -xeuo pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+. $DIR/prep-kube.sh
+cd $GOPATH/src/k8s.io/kubernetes
+make test-e2e-node TEST_ARGS='--report-dir=/tmp/artifacts --kubelet-flags="--cgroup-driver=systemd"' FOCUS="\[Conformance\]"
