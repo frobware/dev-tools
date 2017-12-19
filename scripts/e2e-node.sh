f1189e5886d020cf441652d6c717efc3d0d2eff9
Seth Jennings <sjenning@redhat.com>
2017-12-18 22:19:37 -0600

    converge e2e-node scripts

--- scripts/e2e-node.sh
+++ scripts/e2e-node.sh
@@ -2,7 +2,28 @@
 
 set -xeuo pipefail
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-. $DIR/prep-kube.sh
-cd $GOPATH/src/k8s.io/kubernetes
+GOVERSION=1.9.2
+
+cd $HOME
+yum install gcc -y
+rm -rf /tmp/artifacts/* /tmp/e2e_node* /tmp/run_local*
+if [ ! -d "go$GOVERSION" ]; then
+        curl -OL https://storage.googleapis.com/golang/go$GOVERSION.linux-amd64.tar.gz
+        mkdir $PWD/go$GOVERSION
+        pushd $PWD/go$GOVERSION
+        tar -xzf ../go$GOVERSION.linux-amd64.tar.gz
+        popd
+fi
+GOROOT=$PWD/go$GOVERSION/go
+PATH=$GOROOT/bin:$PATH
+export GOPATH=$PWD/go
+pushd go/src/k8s.io
+if [ ! -d kubernetes ]; then
+	git clone https://github.com/kubernetes/kubernetes.git
+fi
+pushd kubernetes
+git checkout master
+git pull
+git status
+git clean -fdx
 make test-e2e-node TEST_ARGS='--report-dir=/tmp/artifacts --kubelet-flags="--cgroup-driver=systemd"'
