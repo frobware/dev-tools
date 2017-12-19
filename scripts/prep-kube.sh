47de9a21f57d6554748f9c5c64cc43d9354e9268
Seth Jennings <sjenning@redhat.com>
2017-12-18 21:28:54 -0600

    add prep-kube script

---
+++ scripts/prep-kube.sh
@@ -0,0 +1,28 @@
+#/bin/bash
+
+set -xeuo pipefail
+
+GOVERSION=1.9.2
+
+cd $HOME
+yum install gcc -y
+rm -rf /tmp/artifacts/* /tmp/e2e_node* /tmp/run_local*
+if [ ! -d "go$GOVERSION" ]; then
+        curl -OL https://storage.googleapis.com/golang/go$GOVERSION.linux-amd64.tar.gz
+        mkdir $PWD/go$GOVERSION
+        pushd $PWD/go$GOVERSION
+        tar -xzf ../go$GOVERSION.linux-amd64.tar.gz
+        popd
+fi
+GOROOT=$PWD/go$GOVERSION/go
+PATH=$GOROOT/bin:$PATH
+export GOPATH=$PWD/go
+pushd go/src/k8s.io
+if [ ! -d kubernetes ]; then
+	git clone https://github.com/kubernetes/kubernetes.git
+fi
+pushd kubernetes
+git checkout master
+git pull
+git status
+git clean -fdx
